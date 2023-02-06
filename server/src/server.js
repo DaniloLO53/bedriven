d5d99d4a835c109925635945a02c72451c53e4d2
Danilo Leão <olin_ad@hotmail.com>
2023-02-06 18:23:53 -0300

    https certification and key config

--- server/src/server.js
+++ server/src/server.js
@@ -1,17 +1,27 @@
+import fs from 'fs';
+import path from 'path';
 import https from 'https';
 import dotenv from 'dotenv';
 import app from './app.js';
-import mongoose from 'mongoose';
 import { mongoConnect } from './services/mongo.js';
+import { fileURLToPath } from 'url';
 
 dotenv.config();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const PORT = process.env.PORT;
 
 async function startServer() {
   await mongoConnect();
 
-  const server = https.createServer(app);
+  const options = {
+    key: fs.readFileSync('key.pem'),
+    cert: fs.readFileSync('cert.pem')
+  };
+
+  const server = https.createServer(options, app);
   server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 };
 startServer();
